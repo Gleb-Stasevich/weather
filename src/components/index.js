1a5251b6ad22036a94a01bb4bc7a7c526bb6f519
Gleb-Stasevich <gfox.12@mail.ru>
2023-05-09 04:14:43 +0300

    deploy

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import weatherCheckbox from '@/components/weatherCheckbox.vue';
-import weatherInput from '@/components/weatherInput.vue';
-import weatherModal from '@/components/weatherModal.vue';
-import weatherIndicators from '@/components/weatherIndicators.vue';
-
-export default [weatherCheckbox, weatherInput, weatherModal, weatherIndicators];
